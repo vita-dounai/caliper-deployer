356d688c9d23c76a2b1bd40cb83519922bc11191
vita-dounai <44284897+vita-dounai@users.noreply.github.com>
2019-07-19 11:17:16 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,5 @@ echo 'y' | npm run repoclean
 npm run bootstrap
 
 cd packages/caliper-tests-integration/
-npm run teardown_verdaccio
-npm run setup_verdaccio
-npm install --registry http://localhost:4873 -g caliper-cli@0.1.0
+npm run e2e_install
 cd ${cur_dir}
