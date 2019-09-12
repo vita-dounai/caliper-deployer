76493151b0bdb5ce381597346c029179e14e4e5c
Chenxi Li <lichenxi.webank@gmail.com>
2019-09-12 13:04:53 +0800

    adapt to caliper v0.1.0

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 set -e
 set -x
 
-cur_dir=`pwd`
-
 git clone https://github.com/vita-dounai/caliper.git || true
 cd caliper
 git checkout fisco-bcos
 
-npm install
-echo 'y' | npm run repoclean
+npm i
+npm run repoclean -- --yes
 npm run bootstrap
 
 cd packages/caliper-tests-integration/
-npm run e2e_install
-cd ${cur_dir}
+npm run start_verdaccio
+npm run npm_publish_local
+npm install --registry=http://localhost:4873 --only=prod @hyperledger/caliper-cli
+npm run stop_verdaccio
